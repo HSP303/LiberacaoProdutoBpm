1ba0f8fff2eac2468625268afce12c98f2493213
HSP303 <pedrohscheidt@gmail.com>
2025-07-31 16:12:51 -0300

    cockpit no welcome

--- resources/views/components/head-bpm.blade.php
+++ resources/views/components/head-bpm.blade.php
@@ -1,61 +1,59 @@
 <div>
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
-            crossorigin="anonymous"></script>
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.contentWindow.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script type="text/javascript" src="https://cdn.senior.com.br/platform/1.7.1/js/workflow-cockpit.min.js"></script>
-        <script type="text/javascript" src="https://cdn.senior.com.br/bpm/1.10.0/workflow-cockpit.min.js"></script>
-        <script src="./index.js"></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+        crossorigin="anonymous"></script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.contentWindow.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script type="text/javascript" src="https://cdn.senior.com.br/platform/1.7.1/js/workflow-cockpit.min.js"></script>
+    <script type="text/javascript" src="https://cdn.senior.com.br/bpm/1.10.0/workflow-cockpit.min.js"></script>
+    <script src="./index.js"></script>
 
-        <script>
+    <script>
 
-            this.workflowCockpit = workflowCockpit({
-            init: _init,
-            onSubmit: _saveData,
-            onError: _rollback,
-            });
+        this.workflowCockpit = workflowCockpit({
+        init: _init,
+        onSubmit: _saveData,
+        onError: _rollback,
+        });
 
-            function _init(data, info) {
-            console.log("Função _init chamada. Dados iniciais do formulário:", data);
+        function _init(data, info) {
+        console.log("Função _init chamada. Dados iniciais do formulário:", data);
 
-            // Exemplo: Preencher campos com dados iniciais, se disponíveis
-            if (data && data.loadContext && data.loadContext.initialVariables) {
-                const { initialVariables } = data.loadContext;
-                document.getElementById("campoExemplo").value = initialVariables.exemplo || 'Não disponível';
-            }
-            }
+        // Exemplo: Preencher campos com dados iniciais, se disponíveis
+        if (data && data.loadContext && data.loadContext.initialVariables) {
+            const { initialVariables } = data.loadContext;
+            document.getElementById("campoExemplo").value = initialVariables.exemplo || 'Não disponível';
+        }
+        }
 
-            function _rollback(error) {
-            console.error('Erro no workflow. Rollback necessário:', error);
-            // Implemente aqui a lógica de rollback necessária, se aplicável
-            }
+        function _rollback(error) {
+        console.error('Erro no workflow. Rollback necessário:', error);
+        // Implemente aqui a lógica de rollback necessária, se aplicável
+        }
 
-            async function _saveData(newData) {
-            // Constrói o objeto de dados a serem salvos
-            let newData = {
-                cnpj: "teste"
-            };
+        async function _saveData(newData) {
+        // Constrói o objeto de dados a serem salvos
+        let newData = {
+            cnpj: "teste"
+        };
 
-            // Exibe os dados no console para verificação
-            console.log("Dados a serem salvos:", newData);
+        // Exibe os dados no console para verificação
+        console.log("Dados a serem salvos:", newData);
 
-            // Aqui você pode implementar a lógica de envio para o serviço SOAP ou outra integração necessária
+        // Aqui você pode implementar a lógica de envio para o serviço SOAP ou outra integração necessária
 
-            // Retorna os dados para serem enviados ao workflow
-            return {
-                formData: newData,
-            };
-            }
-        </script>
-        
+        // Retorna os dados para serem enviados ao workflow
+        return {
+            formData: newData,
+        };
+        }
+    </script>
+    
 
-        <title>{{ $title }}</title>
-    </head>
+    <title>{{ $title }}</title>
 </div>
