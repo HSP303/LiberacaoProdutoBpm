9ed279be171e9666fea46b09caea6110d9ec63bc
André Will <andre.will@soeltech.com.br>
2025-08-04 17:55:30 -0300

    Table

---
+++ resources/views/components/table_liberacao.blade.php
@@ -0,0 +1,87 @@
+<div class="overflow-auto max-w-full border border-gray-300 rounded-md">
+    <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-200">
+            <tr>
+                <th class="sticky left-0 z-20 bg-gray-200 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    style="left: 0px;">
+                    Item
+                </th>
+                <th class="sticky z-20 bg-gray-200 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    style="left: 200px;">
+                    Especificado
+                </th>
+                <th class="sticky z-20 bg-gray-200 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    style="left: 390px;">
+                    Equipamento
+                </th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Genre</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Duration</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Rating</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Language</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Release Date
+                </th>
+                <th
+                    class="sticky right-0 z-20 bg-gray-200 px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Producer
+                </th>
+            </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-100">
+            <tr>
+                <td class="sticky left-0 z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 0px;">
+                    The Sliding Mr. Bones (Next Stop, Pottersville)
+                </td>
+                <td class="sticky z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 200px;">
+                    Malcolm Lockyer
+                </td>
+                <td class="sticky z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 390px;">
+                    1961
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">View</td>
+                <td class="px-6 py-4 whitespace-nowrap">Jazz</td>
+                <td class="px-6 py-4 whitespace-nowrap">3:45</td>
+                <td class="px-6 py-4 whitespace-nowrap">4.5</td>
+                <td class="px-6 py-4 whitespace-nowrap">English</td>
+                <td class="px-6 py-4 whitespace-nowrap">1961-07-15</td>
+                <td class="sticky right-0 z-10 bg-gray-100 px-6 py-4 whitespace-nowrap">EMI</td>
+            </tr>
+            <tr>
+                <td class="sticky left-0 z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 0px;">
+                    Witchy Woman
+                </td>
+                <td class="sticky z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 200px;">
+                    The Eagles
+                </td>
+                <td class="sticky z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 390px;">
+                    1972
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">View</td>
+                <td class="px-6 py-4 whitespace-nowrap">Rock</td>
+                <td class="px-6 py-4 whitespace-nowrap">4:10</td>
+                <td class="px-6 py-4 whitespace-nowrap">4.7</td>
+                <td class="px-6 py-4 whitespace-nowrap">English</td>
+                <td class="px-6 py-4 whitespace-nowrap">1972-11-08</td>
+                <td class="sticky right-0 z-10 bg-gray-100 px-6 py-4 whitespace-nowrap">Asylum Records</td>
+            </tr>
+            <tr>
+                <td class="sticky left-0 z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 0px;">
+                    Shining Star 
+                </td>
+                <td class="sticky z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 200px;">
+                    Earth, Wind, and Fire
+                </td>
+                <td class="sticky z-10 bg-gray-100 px-6 py-4 whitespace-nowrap" style="left: 390px;">
+                    1975
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">View</td>
+                <td class="px-6 py-4 whitespace-nowrap">Funk</td>
+                <td class="px-6 py-4 whitespace-nowrap">3:20</td>
+                <td class="px-6 py-4 whitespace-nowrap">4.8</td>
+                <td class="px-6 py-4 whitespace-nowrap">English</td>
+                <td class="px-6 py-4 whitespace-nowrap">1975-05-21</td>
+                <td class="sticky right-0 z-10 bg-gray-100 px-6 py-4 whitespace-nowrap">Columbia</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
