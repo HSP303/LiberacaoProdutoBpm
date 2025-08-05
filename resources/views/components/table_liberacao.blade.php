e7cec0c6832642fd2d6a29c98265fba53ee0818c
André Will <andre.will@soeltech.com.br>
2025-08-05 11:52:44 -0300

    Ajuste

--- resources/views/components/table_liberacao.blade.php
+++ resources/views/components/table_liberacao.blade.php
@@ -47,9 +47,6 @@
                 <td class="px-6 py-4 whitespace-nowrap">English</td>
                 <td class="sticky right-0 z-10 bg-gray-100 px-6 py-4 whitespace-nowrap">
                     <form class="max-w-sm mx-auto">
-                        <label for="Seleção"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Selecione</label>
                         <select id="selecao"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option selected>Selecione a Opção</option>
@@ -76,9 +73,6 @@
                 <td class="px-6 py-4 whitespace-nowrap">English</td>
                 <td class="sticky right-0 z-10 bg-gray-100 px-6 py-4 whitespace-nowrap">
                     <form class="max-w-sm mx-auto">
-                        <label for="Seleção"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Selecione</label>
                         <select id="selecao"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option selected>Selecione a Opção</option>
