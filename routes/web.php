19a67ff1bc9744abd4bba83d7fd64949ca875141
André Will <andre.will@soeltech.com.br>
2025-08-06 11:41:06 -0300

    Ajuste

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
