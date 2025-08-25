e17fce5921532325c43b4a9cf414b2ba5aa31262
André Will <andre.will@soeltech.com.br>
2025-08-25 15:35:28 -0300

    Criação da rotina de anexos

--- app/Http/Controllers/AnexosLiberacaoController.php
+++ app/Http/Controllers/AnexosLiberacaoController.php
@@ -17,7 +17,7 @@ class AnexosLiberacaoController extends Controller
             abort(404, 'ID  de liberação não encontrado.');
         }
 
-        return view('files.blade', compact('anexos'));
+        return view('anexos', compact('anexos'));
     }
 
     public function destroy($id, $id_arq){
--- resources/views/anexos.blade.php
+++ resources/views/anexos.blade.php
@@ -12,29 +12,35 @@
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <div class="relative">
-                            <form method="POST" action="{{ route('anexos.store') }}" enctype="multipart/form-data">
+                            <form method="POST" action="{{ route('anexos.store') }}">
                                 @csrf
                                 <div>
                                     <div>
                                         <x-input-label for="descricao_arquivo" :value="__('Descrição do Arquivo')" />
-                                        <x-text-input id="descricao_arquivo" class="block mt-1" style="width:100%;" type="text" name="descricao_arquivo" :value="old('descricao_arquivo')" required autofocus autocomplete="representado" />
+                                        <x-text-input id="descricao_arquivo" class="block mt-1" style="width:100%;"
+                                            type="text" name="descricao_arquivo" :value="old('descricao_arquivo')"
+                                            required autofocus autocomplete="representado" />
                                         <x-input-error :messages="$errors->get('descricao_arquivo')" class="mt-2" />
                                     </div>
                                     <div class="grid grid-cols-1 gap-6 mt-4">
                                         <div class="grid grid-cols-1 gap-6 mt-2">
                                             <div>
-                                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file">Arquivo</label>
-                                                <input accept=".jpg,.jpeg,.png,.pdf" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" name="file" id="file" type="file">
+                                                <label
+                                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                                    for="file">Arquivo</label>
+                                                <input accept=".jpg,.jpeg,.png,.pdf"
+                                                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                                    name="file" id="file" type="file">
                                                 <x-input-error :messages="$errors->get('file')" class="mt-2" />
                                             </div>
                                         </div>
                                     </div>
 
-                                   <div class="flex items-center justify-ini mt-4">
-                                         <x-primary-button>
-                                             {{ __('Cadastrar') }}
-                                         </x-primary-button>
-                                     </div>
+                                    <div class="flex items-center justify-ini mt-4">
+                                        <x-primary-button>
+                                            {{ __('Cadastrar') }}
+                                        </x-primary-button>
+                                    </div>
                                 </div>
                             </form>
                         </div>
@@ -51,26 +57,26 @@
                     <div class="p-6 text-gray-900">
                         <div class="relative">
                             <div class="overflow-x-auto">
-                            <table class="w-full text-sm text-left text-black dark:text-gray-400">
-                                    <thead class="text-xs text-black uppercase rounded-lg" style="background-color: #3b82f6; border-radius: 5px;">
+                                <table class="w-full text-sm text-left text-black dark:text-gray-400">
+                                    <thead class="text-xs text-black uppercase rounded-lg"
+                                        style="background-color: #3b82f6; border-radius: 5px;">
                                         <tr>
                                             <th scope="col" class="px-6 py-3">Sequencia</th>
                                             <th scope="col" class="px-6 py-3">Descrição Arquivo</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach($anexos as $arq)
-                                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100">
-                                                <td class="px-6 py-4">{{ $arq->id_anx }}</td>
-                                                <td class="px-6 py-4">{{ $arq->nome_arquivo }}</td>
-                                            </tr>
-                                        @endforeach
+                                        <tr
+                                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100">
+                                            <td class="px-6 py-4">A</td>
+                                            <td class="px-6 py-4">B</td>
+                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
-                </div> 
+                </div>
             </div>
         </div>
     </div>
