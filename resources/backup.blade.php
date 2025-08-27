1731757d8688f9246154f0739bdf2a723c6b5673
Andre <andrewill07@hotmail.com>
2025-08-27 10:09:13 -0300

    Ajustes no Dashboard

--- resources/backup.blade.php
+++ resources/backup.blade.php
@@ -1,16 +1,14 @@
 <x-app-layout>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Anexos') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <div class="flex flex-col gap-4">
-
-<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="container">
+        <div class="py-3">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <div class="relative">
@@ -99,9 +97,6 @@
                             @endforelse
                         </tbody>
                     </table>
-                </div>
-            </div>
-
                 </div>
             </div>
         </div>
