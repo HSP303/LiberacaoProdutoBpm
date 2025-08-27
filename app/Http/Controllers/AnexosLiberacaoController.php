b344b8d374a552c4e7423aa6d3f67d09b0ae3b5b
Andre <andrewill07@hotmail.com>
2025-08-27 11:44:13 -0300

    Ajustes no Dashboard

--- app/Http/Controllers/AnexosLiberacaoController.php
+++ app/Http/Controllers/AnexosLiberacaoController.php
@@ -7,6 +7,7 @@ use App\Models\AnexosLiberacao;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class AnexosLiberacaoController extends Controller
 {
